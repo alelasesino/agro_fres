cd5c0ff8971d4a4cc33ccc715d895b8b59ce42d7
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-04 17:23:04 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -5,33 +5,28 @@ from odoo import models, fields, api
 
 class Farm(models.Model):
     _name = 'agro.farm'
-    _description = "Farm model"
+    _description = 'Modelo de finca'
+    #_rec_name = 'name'
 
-    display_name = fields.Char(string="Nombre")
-    description = fields.Char(string="Descripción")
-    code = fields.Integer(string="Referencia Interna")
-    farm_ids = fields.One2many('agro.farm.parcel', 'farm_id', 'Parcelas')
+    name = fields.Char(string='Nombre')
+    description = fields.Char(string='Descripción')
+    code = fields.Char(string='Referencia Interna')
+    parcel_ids = fields.One2many('agro.farm.parcel', 'farm_id', string='Parcelas', auto_join=True)
+
+    @api.depends('name', 'code')
+    def name_get(self):
+        res = []
+        for field in self:
+            res.append((field.id, f'[{field.code}] {field.name}'))
+        return res
 
 class Parcel(models.Model):
     _name = 'agro.farm.parcel'
-    _description = "Parcel model"
+    _description = 'Modelo de parcela'
+    #_rec_name = 'name'
 
-    display_name = fields.Char(string="Nombre")
-    description = fields.Char(string="Descripción")
-    number = fields.Integer(string="Numero")
-    farm_id = fields.Many2one('agro.farm', 'Finca')
+    name = fields.Char(string='Nombre')
+    description = fields.Char(string='Descripción')
+    number = fields.Char(string='Número')
+    farm_id = fields.Many2one('agro.farm', 'Finca', ondelete='cascade', auto_join=True)
 
-
-# class webservice(models.Model):
-#     _name = 'webservice.webservice'
-#     _description = 'webservice.webservice'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
